4209ec47b5ed93aaa0174362e6aadeeb02dd6b13
vigor <vigorakbar@gmail.com>
2019-04-22 23:06:03 +0700

    add card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Styled from 'styled-components';
+import cx from 'classnames';
+
+const CardWrapper = Styled.div`
+  max-width: 500px;
+`;
+
+const Card = (props) => {
+  const { src, desc, title, className } = props;
+  return (
+    <CardWrapper className={cx(className, "card")}>
+      <img className="card-img-top" src={src} alt="Project Preview"/>
+      <div className="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p className="card-text">{desc}</p>
+      </div>
+    </CardWrapper>
+  );
+};
+
+export default Card;

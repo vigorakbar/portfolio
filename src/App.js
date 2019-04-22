2078aa01be19852df77c5326e03067102b9eebf7
vigor <vigorakbar@gmail.com>
2019-04-22 23:06:24 +0700

    update layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import Styled from 'styled-components';
+
+// import Navbar from 'components/Navbar'
 import photo from 'assets/vigor.png';
+import test from 'assets/test.png';
+import Card from './components/Card';
 
 const Background = Styled.div`
   background: #18121e;
   height: 100%;
+}
+`;
+
+const Nav = Styled.div`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 20px 40px;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 600;
+  background-color: #233237;
+  color: #eac67a;
 
   .menu-bar {
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-around;
-    color: #eac67a;
-    background-color: #233237;
-    padding: 10px;
-    font-family: 'Raleway', sans-serif;
-    font-size: 18px;
-    font-weight: 500;
+    justify-content: space-between;
+    max-width: 2000px;
+    flex-basis: 500px;
+
+    .main {
+      font-weight: 800;
+      margin-right: 25px;
+    }
   }
-}
 `;
 
 const Cover = Styled.div`
@@ -26,138 +43,157 @@ const Cover = Styled.div`
   justify-content: center;
   overflow: hidden;
   position: relative;
-
   img {
     position: absolute;
     bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
+    right: 10vw;
     max-width: 100%;
-    max-height: 100%;
+    max-height: 80%;
 
-    @media only screen and (max-width: 700px) {
-      margin-right: 10px;
+    @media only screen and (max-width: 750px) {
+      right: -20vw;
     }
 
-    @media only screen and (max-width: 500px) {
-      margin-left: 115px;
+    @media only screen and (max-width: 550px) {
+      right: -30vw;
+    }
+
+    @media only screen and (max-width: 375px) {
+      right: -160px;
     }
   }
 
   .body-wrapper {
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-between;
+    
     margin-top: 30px;
     color: #eac67a;
-    font-size: 25px;
+    font-size: 35px;
   }
 
   .content {
-    margin: 10px;
+    margin: 60px 20px 20px 20px;
 
   .text-title {
     font-family: 'Raleway', sans-serif;
-    font-size: 190px;
+    font-size: 120px;
     font-weight: 600;
     color: white;
 
     @media only screen and (max-width: 1100px) {
-      font-size: 16vw;
+      font-size: 100px;
+    }
+
+    @media only screen and (max-width: 900px) {
+      font-size: 80px;
+    }
+
+    @media only screen and (max-width: 400px) {
+      font-size: 70px;
     }
   }
 
   .profile {
-    width: calc(50% - 110px);
+    width: calc(70% - 110px);
     z-index: 1;
     position: relative;
 
     @media only screen and (max-width: 700px) {
-      width: calc(50% - 35px);
-      font-size: 22px;
+      width: calc(60% - 35px);
+      font-size: 30px;
     }
 
     @media only screen and (max-width: 500px) {
       width: calc(50% - 20px);
-      font-size: 20px;
+      font-size: 22px;
     }
   }
+`;
 
-  .menu-cover {
+const Projects = Styled.div`
+  margin: 20px;
+
+  .title {
     font-family: 'Raleway', sans-serif;
-    font-size: 25px;
-    font-weight: 500;
-    border-radius: 15px;
-    padding: 25px 10px;
-    width: calc(50% - 110px);
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    background-color: #233237;
-    align-items: center;
+    font-weight: 600;
+    font-size: 45px;
+    margin-left: 40px;
+    margin-top: 50px;
 
-    @media only screen and (max-width: 700px) {
-      display: none;
-    }
-
-    > div {
-      margin: 10px 0;
+    @media only screen and (max-width: 500px) {
+      font-size: 40px;
     }
   }
 
-  nav {
-    z-index: 2;
-    display: none;
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
 
-    @media only screen and (max-width: 700px) {
-      display: block;
-    }
+  div {
+    margin-bottom: 50px;
   }
 `;
 
 class App extends Component {
   render() {
     return (
-      <Background>
-        <div className="collapse" id="navbarToggleExternalContent">
+      <div>
+        {/* <Navbar /> */}
+        <Nav>
           <div className="menu-bar">
             <div>PROJECTS</div>
             <div>CONTACT</div>
             <div>CV</div>
           </div>
-        </div>
-        <Cover>
-          <img src={photo} alt="portrait of Vigor" />
-          <div className="content">
-            <nav className="navbar navbar-dark">
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-            </nav>
-            <div className="text-title">
-              Vigor Akbar
-            </div>
-            <div className="body-wrapper">
-              <div className="profile">
-                <div>I'm interested in web development especially on Front-end web development.</div>
-                <div></div>
+        </Nav>
+        <Background>
+          <Cover>
+            <img src={photo} alt="portrait of Vigor" />
+            <div className="content">
+              <div className="text-title">
+                Hi, I'm <br /> Vigor Akbar
               </div>
-              {/* <div className="menu-cover">
-                <div>PROJECTS</div>
-                <div>CONTACT</div>
-                <div>CV</div>
-              </div> */}
+              <div className="body-wrapper">
+                <div className="profile">
+                  I'm a web developer, focused on front-end side of the web.
+                </div>
+              </div>
             </div>
-          </div>
-        </Cover>
-        <div>TESST</div><br />
-        <div>TESST</div><br />
-        <div>TESST</div><br />
-        <div>TESST</div><br />
-        <div>TESST</div><br />
-
-      </Background>
+          </Cover>
+          <Projects>
+            <div className="title">
+              <div>
+                Projects and Works
+              </div>
+            </div>
+            <div className="container">
+              <Card
+                src={test}
+                title="Test 1"
+                desc="teststs"
+              />
+              <Card
+                src={test}
+                title="Test 1"
+                desc="teststs"
+              />
+              <Card
+                src={test}
+                title="Test 1"
+                desc="teststs"
+              />
+              <Card
+                src={test}
+                title="Test 1"
+                desc="teststs"
+              />
+            </div>
+          </Projects>
+        </Background>
+      </div>
     );
   }
 }
